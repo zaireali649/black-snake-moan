4ce1b77c4f4b78d389c9a79fb63eef6b2321f5d1
Zaire Ali <zaireali649@gmail.com>
2023-01-04 00:30:36 +0000

    Lists

---
+++ lists.py
@@ -0,0 +1,27 @@
+var = []
+
+print(var)
+print(type(var))
+
+var_2 = [151, 251, 386, 493, "009"]
+
+print(var_2)
+
+var_2.append(649)
+
+print(var_2)
+
+#print(dir(var_2))
+
+numbers = [0, 1, 2, 3, 4]
+
+for number in numbers:
+    print(number*number)
+    
+numbers2 = list(range(0,100))
+
+print(numbers2)
+
+print(len(numbers2))
+
+print(numbers2[50])
